3054eee2539d6c20ef749facc1792b152cdec60d
Martin <mail@mvb1.de>
2020-09-18 07:40:13 +0200

    img srcset sizes und alt text anpassung

---
+++ wp-recent-post-slider-enq.php
@@ -0,0 +1,23 @@
+<?php
+add_action('wp_enqueue_scripts', 'wp_recent_post_scripts');
+
+function wp_recent_post_scripts()
+{
+  wp_reset_query();
+  $plugin_url = plugins_url('/', __FILE__);
+
+  if (is_front_page() || is_home() || (0==0)) {
+    //If page is using slider portfolio template then load our slider script
+    // Load Styles
+
+    wp_enqueue_style('wp_recent_post_style1', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css');
+    wp_enqueue_style('wp_recent_post_style2', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css');
+    wp_enqueue_style('wp_recent_post_style3', $plugin_url . 'css/wp-recent-post-slider.css');
+   
+
+    // Load Scripts
+    wp_enqueue_script('wp_recent_post_script1', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js',array('jquery'), '1.10.2', true);
+    wp_enqueue_script('wp_recent_post_script2', $plugin_url . 'js/wp-recent-post-slider.js', array('jquery'), '1.10.2', true);
+   
+  }
+}
--- wp-recent-post-slider.php
+++ wp-recent-post-slider.php
@@ -4,13 +4,13 @@
  *
  * @link              www.mvb1.de
  * @since             1.0.0
- * @package           Wp_post_map_view_simple
+ * @package           wp-recent-post-slider
  *
  * @wordpress-plugin
- * Plugin Name:       wp_post_map_view_simple
+ * Plugin Name:       wp-recent-post-slider
  * Plugin URI:        www.mvb1.de
- * Description:       Anzeige aller Posts (max 100!) mit GPS-Daten (lat, lon) und Kategorie in einer Karte
- * Version:           0.3.0
+ * Description:       Anzeige der letzten Posts in einem Slider mit Shortcode: [recent-post-slider numberposts="5"]
+ * Version:           0.1.0
  * Author:            Martin von Berg
  * Author URI:        www.mvb1.de
  * License:           GPL-2.0+
@@ -19,102 +19,126 @@
 
 defined('ABSPATH') or die('Are you ok?');
 
-add_shortcode('mapview', 'show_post_map');
+add_shortcode('recent-post-slider', 'show_recent_posts');
 
 // Wortlänge für excerpt vorgeben. Gilt für alle Excerpts auf der ganzen Seite!!!
-function wp_example_excerpt_length( $length ) {
-    return 50; // Anzahl Worte!
-}
-add_filter( 'excerpt_length', 'wp_example_excerpt_length');
+//function wp_slider_excerpt_length( $length ) {
+//    return 50; // Anzahl Worte!
+//}
+//add_filter( 'excerpt_length', 'wp_slider_excerpt_length');
 
-function show_post_map($attr)
+function show_recent_posts($attr)
 {
 	global $wpdb;
 	
 	// Pfade und Verzeichnisse definieren
-	$plugin_path = $plugin_url = plugins_url('/', __FILE__);
-	$wp_postmap_path = $plugin_path . 'images/';
+	//$plugin_path = $plugin_url = plugins_url('/', __FILE__);
+	//$wp_postmap_path = $plugin_path . 'images/';
 	
-	$args = array('numberposts' => 100); 
-	$custom_posts = get_posts($args);
+	// Parameter extrahieren und vordefinieren
+	extract(shortcode_atts(array(
+		'numberposts' => 5,
+	), $attr));
+
+	$custom_posts = get_posts($attr);
 	$i = 0;
 	$string = '';
-	$string .= '<div class="box1">';
-	$string .= '<div id="map"></div>'; // hier wird die Karte erzeugt!
-	$string .= '<div id="map10_img">';
+	$dateformat = get_option('date_format');
 	
+	$string .= '<div class="rps-wrapper">';
+	$string .= '<div class="rps-carousel">';
+	
+	$min_width = 300;
+	$max_width = 600;
+
+	$imgsizes = "(max-width: 480px) 100vw, (max-width: 700px) 50vw, (max-width: 1024px) 33vw, 350px"; //abgeleitet aus dem javascript für slick
+
+	$upload_dir    = wp_get_upload_dir();
+		
 	// Post auslesen und <a>-Tags mit allen Informationen schreiben
 	
 	foreach ($custom_posts as $post) { 
-		$lat = get_post_meta($post->ID, 'lat', true);
-		$lon = get_post_meta($post->ID, 'lon', true);
-		
-		if (!((is_null($lat) || (0 == $lat)) && (is_null($lon) || (0 == $lon)))) { // Achte auf das Not!
 			$title = substr($post->post_title,0,80); // Länge des Titels beschränken, Anzahl Zeichen
-			$tags= get_the_tags($post->ID);
-			$tagnames = array();
+			$featimage = get_the_post_thumbnail_url($post->ID, $size='thumbnail'); 
+			$featured_image_id = get_post_thumbnail_id( $post->ID );
+			$image_meta = wp_get_attachment_metadata( $featured_image_id );
 			
-			if ($tags !== false) {
-				foreach ($tags as $tag) {
-					$tagnames[] = $tag->name;
+			$my_srcset = '';
+
+			if ( is_array($image_meta) ) {
+				// Retrieve the uploads sub-directory from the full size image.
+				$dirname = _wp_get_attachment_relative_path( $image_meta['file'] );
+
+				if ( $dirname ) {
+					$dirname = trailingslashit( $dirname );
 				}
-			} 
-						
-			$tag2 = implode(" ",$tagnames);
-			$icon = wp_postmap_get_icon($tag2);
+				
+				$image_baseurl = trailingslashit( $upload_dir['baseurl'] ) . $dirname;
+
+				/*
+				* If currently on HTTPS, prefer HTTPS URLs when we know they're supported by the domain
+				* (which is to say, when they share the domain name of the current request).
+				*/
+				if ( is_ssl() && 'https' !== substr( $image_baseurl, 0, 5 ) && parse_url( $image_baseurl, PHP_URL_HOST ) === $_SERVER['HTTP_HOST'] ) {
+					$image_baseurl = set_url_scheme( $image_baseurl, 'https' );
+				}
+
+				$image_sizes = $image_meta['sizes'];
+				
+				usort($image_sizes, 'cmp');
+
+				foreach ($image_sizes as $image) {
+					$width = $image['width'];
+					
+					if (($width >= $min_width) & ($width <= $max_width) & ($image['mime-type'] == 'image/jpeg')) { 
+						$my_srcset .= $image_baseurl . $image['file'] . ' ' . $image['width'] . 'w, ';
+					}
+				}
+				$my_srcset = rtrim($my_srcset, ', '); 
+				if ( ! empty($my_srcset)) {
+					$my_srcset = 'srcset="' . $my_srcset . '" sizes="' . $imgsizes . '"';
+				}
+			}
+
+			$postlink = get_permalink($post->ID);
+			$category = get_the_category($post->ID)[0]->cat_name;
+			$date = get_the_date($dateformat,$post->ID);
 			$excerpt = get_the_excerpt($post->ID);
 			
 			if (strpos($excerpt, 'urzbeschr') !== false) { 
 				$excerpt = ltrim(strstr($excerpt," "));
 			}
 
-			$featimage = get_the_post_thumbnail_url($post->ID, $size='thumbnail'); 
-			$postlink = get_permalink($post->ID);
+			if (strpos($excerpt, '<a') !== false) { 
+				$posalttag = strpos($excerpt,"<a");
+				$excerpt = substr($excerpt,0,$posalttag);
+			}
+			
 			$i++;
-			$string  .= '<a alt="' . $title . '" href="' . $featimage . '" data-title="'.$title.'" data-icon="'. $icon. '" data-geo="lat:' . $lat . ',lon:' . $lon . '" data-link="'. $postlink .'">' . $excerpt. '</a>';
-		}
+			$string .= '<div class ="rps-blog-cart">';
+			$string .= '<a href = "' . $postlink . '">';
+			$string .= '<img loading="lazy" ' . $my_srcset . ' src="'. $featimage .'" alt="' . $category . ' '. $title .'">';
+			$string .= '<p class="rps-category">' . $category . '</p>';
+			$string .= '<p class="rps-date">' . $date . '</p>';
+			$string .= '<h4 class="rps-title">' . $title . '</h4>';
+			$string .= '<p class="rps-excerpt">' . $excerpt . '</p>';
+			$string .= '<button class="rps-button">Weiterlesen...</button></a>';
+			$string .= '</div>';
+		
 	}
 	
 	$string  .= '</div></div>';
-	$string  .= '<script>var g_wp_postmap_path = "' . $wp_postmap_path . '";'; 
-	$string  .= '</script>';
+	
 	return $string;
 }
 
-require_once __DIR__ . '/wp_post_map_view_simple_enq.php';
+require_once __DIR__ . '/wp-recent-post-slider-enq.php';
 
-function wp_postmap_get_icon($arraytagnames)
-{
-	$icon = "";
-	switch (true){
-		case stristr($arraytagnames,'Trekk') !== false:
-		   $icon = "hiking";
-		   break;
-		case ((stristr($arraytagnames,'bike') !== false) && (stristr($arraytagnames,'hike') !== false)):
-		   $icon = "bike-hike";
-		   break;
-		case stristr($arraytagnames,'Radfahren') !== false:
-			$icon = "cycling";
-			break;   
-		case stristr($arraytagnames,'MTB') !== false:
-			$icon = "MTB";
-			break;	
-		case stristr($arraytagnames,'Wander') !== false:
-			$icon = "hiking";
-			break;
-		case stristr($arraytagnames,'Bergtour') !== false:
-			$icon = "mountain";
-			break;	
-		case stristr($arraytagnames,'skitour') !== false:
-			$icon = "skiing"	;
-			break;
-		case stristr($arraytagnames,'reisebericht') !== false:
-			$icon = "travel";
-			break;	
-		default:
-			$icon = "travel";
-		break;	
-	 }
-	 
-	 return $icon;
-}
+function cmp($a, $b)
+	{
+		if ($a['width'] == $b['width']) {
+			return 0;
+		}
+		return ($a['width'] < $b['width']) ? -1 : 1;
+		
+	}
