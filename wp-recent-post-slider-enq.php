665c57fc2baf6823e704a825d8df2c2781d6ad9a
Martin <mail@mvb1.de>
2021-04-01 10:02:09 +0200

    transient introduced

--- wp-recent-post-slider-enq.php
+++ wp-recent-post-slider-enq.php
@@ -5,20 +5,17 @@ function wp_recent_post_scripts()
 {
   wp_reset_query();
   $plugin_url = plugins_url('/', __FILE__);
-
-  if (is_front_page() || is_home()) {
-    //If page is using slider portfolio template then load our slider script
-    // Load Styles
-    //wp_enqueue_style('wp_recent_post_style1', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css');
-    //wp_enqueue_style('wp_recent_post_style2', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css');
-    wp_enqueue_style('wp_recent_post_style1', $plugin_url . 'css/slick.min.css');
-    wp_enqueue_style('wp_recent_post_style2', $plugin_url . 'css/slick-theme.min.css');
-    wp_enqueue_style('wp_recent_post_style3', $plugin_url . 'css/wp-recent-post-slider.min.css');
-   
-
-    // Load Scripts
-    //wp_enqueue_script('wp_recent_post_script1', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js',array('jquery'), '1.10.2', true);
-    wp_enqueue_script('wp_recent_post_script1', $plugin_url . 'js/slick.min.js',array('jquery'), '1.10.2', true);
-    wp_enqueue_script('wp_recent_post_script2', $plugin_url . 'js/wp-recent-post-slider.min.js', array('jquery'), '1.10.2', true);
-  }
+ 
+  //If page is using slider portfolio template then load our slider script
+  // Load Styles
+  //wp_enqueue_style('wp_recent_post_style1', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css');
+  //wp_enqueue_style('wp_recent_post_style2', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css');
+  wp_enqueue_style('wp_recent_post_style1', $plugin_url . 'css/slick.min.css', '', '1.8.0');
+  wp_enqueue_style('wp_recent_post_style2', $plugin_url . 'css/slick-theme.min.css', '', '1.8.0');
+  wp_enqueue_style('wp_recent_post_style3', $plugin_url . 'css/wp-recent-post-slider.min.css');
+  
+  // Load Scripts
+  //wp_enqueue_script('wp_recent_post_script1', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js',array('jquery'), '1.10.2', true);
+  wp_enqueue_script('wp_recent_post_script1', $plugin_url . 'js/slick.min.js',array('jquery'), '1.8.0', true);
+  wp_enqueue_script('wp_recent_post_script2', $plugin_url . 'js/wp-recent-post-slider.min.js', array('jquery'), '0.0.1', true);
 }
--- wp-recent-post-slider.php
+++ wp-recent-post-slider.php
@@ -18,125 +18,146 @@
  * PHP-Version:		  5.4+, tested with PHP 8.0
  */
 
- // namespace definition fehlt hier!
+namespace mvbplugins\recentpostslider;
 
 defined('ABSPATH') or die('Are you ok?');
 
-add_shortcode('recent-post-slider', 'show_recent_posts');
+add_shortcode('recent-post-slider', '\mvbplugins\recentpostslider\show_recent_posts');
 
 function show_recent_posts($attr)
 {
-	// Parameter extrahieren und vordefinieren
-	extract(shortcode_atts(array(
-		'numberposts' => 5,
-	), $attr));
+	// check the transient set time and delete transient if post was published during that time
+	$transient_duration = \WEEK_IN_SECONDS;
+	$last_post_date = \get_lastpostdate('server', 'post'); // "2020-12-24 13:16:03.000000"
+	$last_post_date = \strtotime( $last_post_date ); // now in seconds from 01.01.1970 00:00:00.000
+	$expires = (int) get_option( '_transient_timeout_recentpostslider_html_output', 0 ); // int value 0 if not set
+	$transient_set_time = $expires - $transient_duration;
 
-	$custom_posts = get_posts($attr);
-	$dateformat = get_option('date_format');
+	if ( ($last_post_date > $transient_set_time) || \current_user_can( 'administrator' )) {
+		\delete_transient( 'recentpostslider_html_output' );
+	}
 
-	$string = '';
-	$string .= '<div class="rps-wrapper">';
-	$string .= '<div class="rps-carousel">';
+	// generate the output if not set in transient
+	if ( false === ( $string = get_transient( 'recentpostslider_html_output' ) ) ) {
+		// Parameter extrahieren und vordefinieren
+		extract(shortcode_atts(array(
+			'numberposts' => 5,
+		), $attr));
 
-	$min_width = 200;
-	$max_width = 700;
+		$custom_posts = get_posts($attr);
+		$dateformat = get_option('date_format');
 
-	$imgsizes = "(max-width: 480px) 100vw, (max-width: 700px) 50vw, (max-width: 1024px) 33vw, 350px"; //abgeleitet aus dem javascript für slick
+		$string = '';
+		$string .= '<div class="rps-wrapper">';
+		$string .= '<div class="rps-carousel">';
 
-	$upload_dir    = wp_get_upload_dir();
+		$min_width = 200;
+		$max_width = 700;
 
-	$lenexcerpt = 300;
-	$lentitle = 80;
-	
-	foreach ($custom_posts as $post) { 
-			$title = substr($post->post_title,0, $lentitle); // Länge des Titels beschränken, Anzahl Zeichen
-			$featimage = get_the_post_thumbnail_url($post->ID, $size='thumbnail'); 
-			$featured_image_id = get_post_thumbnail_id( $post->ID );
-			$image_meta = wp_get_attachment_metadata( $featured_image_id );
-			$content = $post->post_content;
-			$postlink = get_permalink($post->ID); // guid vorhanden, aber kein schöner permalink
-			$category = get_the_category($post->ID)[0]->cat_name;
-			$date = get_the_date($dateformat,$post->ID); // $post->post_date
-			
-			// Excerpt nur aus den Absätzen <p> herstellen! Schlüsselwörter entfernen, dürfen dann im Text nicht vorkommen
-			// Absätze mit [shortcodes] werden ignoriert.
-			// der html-code muss mit zeilenumbrüchen formatiert sein, sonst geht das nicht!
-			$p = '';
-			foreach(preg_split("/((\r?\n)|(\r\n?))/", $content) as $line){ 
-				$sub = substr($line,0,3); // html-tag aus der zeile ausschneiden
-				$isshortcode = strpos($line,'['); 
-				if (($sub == '<p>') and ($isshortcode == false)) {
-					$p .= substr($line,3);
-				}
-				$p = str_replace('</p>','',$p);
-			} 
-			$p = str_replace('Kurzbeschreibung:','',$p);
-			$p = str_replace('Tourenbeschreibung:','',$p);
-			$p = strip_tags($p); // html-Tags entfernen
-			$p = substr($p,0, $lenexcerpt); // erst jetzt auf die richtige länge kürszen
-			$excerpt = $p . '...';
+		$imgsizes = "(max-width: 480px) 100vw, (max-width: 700px) 50vw, (max-width: 1024px) 33vw, 350px"; //abgeleitet aus dem javascript für slick
 
-			// special imgsrcset für slick erstellen, dass Standard srcset von WP kann nicht genutzt werden!
-			$my_srcset = '';
-			if ( is_array($image_meta) ) {
-				// Retrieve the uploads sub-directory from the full size image.
-				$dirname = _wp_get_attachment_relative_path( $image_meta['file'] );
+		$upload_dir    = wp_get_upload_dir();
 
-				if ( $dirname ) {
-					$dirname = trailingslashit( $dirname );
-				}
+		$lenexcerpt = 300;
+		$lentitle = 80;
+		
+		foreach ($custom_posts as $post) { 
+				$title = substr($post->post_title,0, $lentitle); // Länge des Titels beschränken, Anzahl Zeichen
+				$featimage = get_the_post_thumbnail_url($post->ID, $size='thumbnail'); 
+				$featured_image_id = get_post_thumbnail_id( $post->ID );
+				$image_meta = wp_get_attachment_metadata( $featured_image_id );
+				$content = $post->post_content;
+				$postlink = get_permalink($post->ID); // guid vorhanden, aber kein schöner permalink
+				$category = get_the_category($post->ID)[0]->cat_name;
+				$date = get_the_date($dateformat,$post->ID); // $post->post_date
 				
-				$image_baseurl = trailingslashit( $upload_dir['baseurl'] ) . $dirname;
+				// Excerpt nur aus den Absätzen <p> herstellen! Schlüsselwörter entfernen, dürfen dann im Text nicht vorkommen
+				// Absätze mit [shortcodes] werden ignoriert.
+				// der html-code muss mit zeilenumbrüchen formatiert sein, sonst geht das nicht!
+				$p = '';
+				foreach(preg_split("/((\r?\n)|(\r\n?))/", $content) as $line){ 
+					$sub = substr($line,0,3); // html-tag aus der zeile ausschneiden
+					$isshortcode = strpos($line,'['); 
+					if (($sub == '<p>') and ($isshortcode == false)) {
+						$p .= substr($line,3);
+					}
+					$p = str_replace('</p>','',$p);
+				} 
+				$p = str_replace('Kurzbeschreibung:','',$p);
+				$p = str_replace('Tourenbeschreibung:','',$p);
+				$p = strip_tags($p); // html-Tags entfernen
+				$p = substr($p,0, $lenexcerpt); // erst jetzt auf die richtige länge kürszen
+				$excerpt = $p . '...';
 
-				/*
-				* If currently on HTTPS, prefer HTTPS URLs when we know they're supported by the domain
-				* (which is to say, when they share the domain name of the current request).
-				*/
-				if ( is_ssl() && 'https' !== substr( $image_baseurl, 0, 5 ) && parse_url( $image_baseurl, PHP_URL_HOST ) === $_SERVER['HTTP_HOST'] ) {
-					$image_baseurl = set_url_scheme( $image_baseurl, 'https' );
-				}
+				// special imgsrcset für slick erstellen, dass Standard srcset von WP kann nicht genutzt werden!
+				$my_srcset = '';
+				if ( is_array($image_meta) ) {
+					// Retrieve the uploads sub-directory from the full size image.
+					$dirname = _wp_get_attachment_relative_path( $image_meta['file'] );
 
-				$image_sizes = $image_meta['sizes'];
-				
-				usort($image_sizes, 'cmp');
-
-				foreach ($image_sizes as $image) {
-					$width = $image['width'];
+					if ( $dirname ) {
+						$dirname = trailingslashit( $dirname );
+					}
 					
-					if (($width >= $min_width) & ($width <= $max_width) & ($image['mime-type'] == 'image/jpeg')) { 
-						$my_srcset .= $image_baseurl . $image['file'] . ' ' . $image['width'] . 'w, ';
+					$image_baseurl = trailingslashit( $upload_dir['baseurl'] ) . $dirname;
+
+					/*
+					* If currently on HTTPS, prefer HTTPS URLs when we know they're supported by the domain
+					* (which is to say, when they share the domain name of the current request).
+					*/
+					if ( is_ssl() && 'https' !== substr( $image_baseurl, 0, 5 ) && parse_url( $image_baseurl, PHP_URL_HOST ) === $_SERVER['HTTP_HOST'] ) {
+						$image_baseurl = set_url_scheme( $image_baseurl, 'https' );
+					}
+
+					$image_sizes = $image_meta['sizes'];
+					
+					usort($image_sizes, '\mvbplugins\recentpostslider\cmp');
+
+					foreach ($image_sizes as $image) {
+						$width = $image['width'];
+						
+						if (($width >= $min_width) & ($width <= $max_width) & ($image['mime-type'] == 'image/jpeg')) { 
+							$my_srcset .= $image_baseurl . $image['file'] . ' ' . $image['width'] . 'w, ';
+						}
+					}
+					$my_srcset = rtrim($my_srcset, ', '); 
+					if ( ! empty($my_srcset)) {
+						$my_srcset = 'srcset="' . $my_srcset . '" sizes="' . $imgsizes . '"';
 					}
 				}
-				$my_srcset = rtrim($my_srcset, ', '); 
-				if ( ! empty($my_srcset)) {
-					$my_srcset = 'srcset="' . $my_srcset . '" sizes="' . $imgsizes . '"';
-				}
-			}
 
-			$string .= '<div class ="rps-blog-cart">';
-			$string .= '<a href = "' . $postlink . '">';
-			$string .= '<img loading="lazy" ' . $my_srcset . ' src="'. $featimage .'" alt="' . $category . ' '. $title .'">';
-			$string .= '<p class="rps-category">' . $category . '</p>';
-			$string .= '<p class="rps-date">' . $date . '</p>';
-			$string .= '<h4 class="rps-title">' . $title . '</h4>';
-			$string .= '<p class="rps-excerpt">' . $excerpt . '</p>';
-			$string .= '<button class="rps-button">Weiterlesen...</button></a>';
-			$string .= '</div>';
+				$string .= '<div class ="rps-blog-cart">';
+				$string .= '<a href = "' . $postlink . '">';
+				$string .= '<img loading="lazy" ' . $my_srcset . ' src="'. $featimage .'" alt="' . $category . ' '. $title .'">';
+				$string .= '<p class="rps-category">' . $category . '</p>';
+				$string .= '<p class="rps-date">' . $date . '</p>';
+				$string .= '<h4 class="rps-title">' . $title . '</h4>';
+				$string .= '<p class="rps-excerpt">' . $excerpt . '</p>';
+				$string .= '<button class="rps-button">Weiterlesen...</button></a>';
+				$string .= '</div>';
+		}
+		
+		$string  .= '</div></div>';
+		// end generation of html output: write the html-output in $string now as get_transient
+		\set_transient('post_map_html_output', $string, $transient_duration);
 	}
-	
-	$string  .= '</div></div>';
-	
+
 	return $string;
 }
 
 require_once __DIR__ . '/wp-recent-post-slider-enq.php';
 
-// Vergleichsfunktion
-function cmp($a, $b)
-	{
-		if ($a['width'] == $b['width']) {
-			return 0;
-		}
-		return ($a['width'] < $b['width']) ? -1 : 1;
+/**
+ * Vergleichsfunktion für 'width' in zwei Arrays
+ *
+ * @param array $a array1 for comparison
+ * @param array $b array2 for comparison
+ * @return integer result as integer: -1 or 1
+ */
+function cmp($a, $b) {
+	if ($a['width'] == $b['width']) {
+		return 0;
+	}
+	return ($a['width'] < $b['width']) ? -1 : 1;
 		
 	}
